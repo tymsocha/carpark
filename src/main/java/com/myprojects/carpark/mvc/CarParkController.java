9d5ca714d82c8f392bb0d29af88b73c0ddc5cd83
tymsocha <tymoteusz.socha@outlook.com>
2020-09-20 21:55:47 +0200

    Comments in controller added

--- src/main/java/com/myprojects/carpark/mvc/CarParkController.java
+++ src/main/java/com/myprojects/carpark/mvc/CarParkController.java
@@ -5,13 +5,24 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+//Klasa będąca kontrolerem aplikacji, która ma bezpośrednie połączenie z frontową częscią aplikacji.
+//Wysyła i odbiera dane z interfejsu użytkownika w postaci URLi i dodatkowych parametrów
+//Tutaj są implementowane tzw. endpointy czyli punkty końcowe aplikacji, do których można się odwołać najczęsciej po to aby pobrać dane.
+//@RestController - adnotacja reprezentująca komponentu springowego zajmującego się tworzeniem endpointów
+//@CrossOrigin - adnotacja umożliwiająca połączenie pomiędzy aplikacją kliencką a aplikacją od strony serwerowej
+//@AllArgsConstructor - Adnotacja z biblioteki Lombok, tworząca konstruktor wykorzystująca wszystkie pola klasy
 @RestController
 @CrossOrigin
 @AllArgsConstructor
 public class CarParkController {
 
+    //Pola powstałe dzięki utworzeniu beanów w danych klasach
     CarParkApi carParkApi;
 
+    //@RequestMapping - adnotacja, w której określana jest metoda RESTowa (np.: POST, GET, DELETE), a także url, dzięki któremu tą metodę można wykorzystać w aplikacji klienckiej
+    //Klasa ResponseEntity jest użyta specjalnie na potrzeby aplikacji restowych, gdzie można określić inee parametry jak np.: kod odpowiedzi
+    //@PathVariable - adnotacja określająca zmienną zawartą w ścieżce do endpointa, jest ona wymagana
+    //Metoda ta generuje miejsca parkingowe i zapisuje dane w bazie
     @RequestMapping(method = RequestMethod.POST, value = "floor/{floors}/spots/{spots}")
     public ResponseEntity<?> generateCarPark(
             @PathVariable("floors") Integer floors,
@@ -20,11 +31,17 @@ public class CarParkController {
         return ResponseEntity.ok(carParkApi.generateSlots(floors, spots));
     }
 
+    //@RequestMapping - adnotacja, w której określana jest metoda RESTowa (np.: POST, GET, DELETE), a także url, dzięki któremu tą metodę można wykorzystać w aplikacji klienckiej
+    //Klasa ResponseEntity jest użyta specjalnie na potrzeby aplikacji restowych, gdzie można określić inee parametry jak np.: kod odpowiedzi
     @RequestMapping(method = RequestMethod.GET, value = "check")
     public ResponseEntity<?> checkIfCarParkGenerated() {
         return ResponseEntity.ok(carParkApi.checkIfSpotsGenerated());
     }
 
+    //@RequestMapping - adnotacja, w której określana jest metoda RESTowa (np.: POST, GET, DELETE), a także url, dzięki któremu tą metodę można wykorzystać w aplikacji klienckiej
+    //Klasa ResponseEntity jest użyta specjalnie na potrzeby aplikacji restowych, gdzie można określić inee parametry jak np.: kod odpowiedzi
+    //@RequestParam - adnotacja określająca parametry opcjonalne przekazywane do endpointa
+    //Metoda zwracająca listę zajętych miejsc danym czasie
     @RequestMapping(method = RequestMethod.GET, value = "occupiedSlots")
     public ResponseEntity<?> getSpotsOccupiedInTimePeriod(
             @RequestParam String timeString,
@@ -33,6 +50,11 @@ public class CarParkController {
        return ResponseEntity.ok(carParkApi.getOccupiedSlots(timeString, floor));
     }
 
+    //@RequestMapping - adnotacja, w której określana jest metoda RESTowa (np.: POST, GET, DELETE), a także url, dzięki któremu tą metodę można wykorzystać w aplikacji klienckiej
+    //Klasa ResponseEntity jest użyta specjalnie na potrzeby aplikacji restowych, gdzie można określić inee parametry jak np.: kod odpowiedzi
+    //@RequestParam - adnotacja określająca parametry opcjonalne przekazywane do endpointa
+    //@PathVariable - adnotacja określająca zmienną zawartą w ścieżce do endpointa, jest ona wymagana
+    //Metoda zwracająca statystyki przez ile czasu dane miejsce było zajęte
     @RequestMapping(method = RequestMethod.GET, value = "occupation/floor/{floor}")
     public ResponseEntity<?> getOccupationTimeAmount(
             @PathVariable("floor") Integer floor,
@@ -42,6 +64,11 @@ public class CarParkController {
         return ResponseEntity.ok(carParkApi.getAmountOfOccupationTimeForSlot(floor, startDate, endDate));
     }
 
+    //@RequestMapping - adnotacja, w której określana jest metoda RESTowa (np.: POST, GET, DELETE), a także url, dzięki któremu tą metodę można wykorzystać w aplikacji klienckiej
+    //Klasa ResponseEntity jest użyta specjalnie na potrzeby aplikacji restowych, gdzie można określić inee parametry jak np.: kod odpowiedzi
+    //@RequestParam - adnotacja określająca parametry opcjonalne przekazywane do endpointa
+    //@PathVariable - adnotacja określająca zmienną zawartą w ścieżce do endpointa, jest ona wymagana
+    //Metoda zwracająca statystyki, przez ile czasu dane miejsce było zajęte, jakie było zużycie energii na dane miejsce i jego koszt
     @RequestMapping(method = RequestMethod.GET, value = "energyConsumption/spot/{spot}")
     public ResponseEntity<?> getElectricityConsumptionPerSpotAndCost(
             @PathVariable("spot") String spot,
@@ -53,6 +80,11 @@ public class CarParkController {
         return ResponseEntity.ok(carParkApi.getElectricityConsumptionAndCost(spot, energyConsumption, cost, startDate, endDate));
     }
 
+    //@RequestMapping - adnotacja, w której określana jest metoda RESTowa (np.: POST, GET, DELETE), a także url, dzięki któremu tą metodę można wykorzystać w aplikacji klienckiej
+    //Klasa ResponseEntity jest użyta specjalnie na potrzeby aplikacji restowych, gdzie można określić inee parametry jak np.: kod odpowiedzi
+    //@RequestParam - adnotacja określająca parametry opcjonalne przekazywane do endpointa
+    //@PathVariable - adnotacja określająca zmienną zawartą w ścieżce do endpointa, jest ona wymagana
+    //Metoda zwracająca statystyki, przez ile czasu dane miejsce było zajęte, jakie było zużycie energii na dane miejsce i jego koszt, dla danego piętra i przedziału czasowego
     @RequestMapping(method = RequestMethod.GET, value = "energyConsumption/floor/{floor}")
     public ResponseEntity<?> getElectricityConsumptionPerFloorAndCost(
             @PathVariable("floor") Integer floor,
@@ -64,6 +96,10 @@ public class CarParkController {
         return ResponseEntity.ok(carParkApi.getElectricityConsumptionAndCostPerFloor(floor, energyConsumption, cost, startDate, endDate));
     }
 
+    //@RequestMapping - adnotacja, w której określana jest metoda RESTowa (np.: POST, GET, DELETE), a także url, dzięki któremu tą metodę można wykorzystać w aplikacji klienckiej
+    //Klasa ResponseEntity jest użyta specjalnie na potrzeby aplikacji restowych, gdzie można określić inee parametry jak np.: kod odpowiedzi
+    //@RequestParam - adnotacja określająca parametry opcjonalne przekazywane do endpointa
+    //Metoda zwracająca statystyki, przez ile czasu dane miejsce było zajęte, jakie było zużycie energii na dane miejsce i jego koszt, dla całego parkingu i przedziału czasowego
     @RequestMapping(method = RequestMethod.GET, value = "energyConsumption")
     public ResponseEntity<?> getElectricityConsumptionForCarPark(
             @RequestParam Long energyConsumption,
@@ -74,6 +110,11 @@ public class CarParkController {
         return ResponseEntity.ok(carParkApi.getElectricityConsumptionAndCostForCarPark(energyConsumption, cost, startDate, endDate));
     }
 
+    //@RequestMapping - adnotacja, w której określana jest metoda RESTowa (np.: POST, GET, DELETE), a także url, dzięki któremu tą metodę można wykorzystać w aplikacji klienckiej
+    //Klasa ResponseEntity jest użyta specjalnie na potrzeby aplikacji restowych, gdzie można określić inee parametry jak np.: kod odpowiedzi
+    //@RequestParam - adnotacja określająca parametry opcjonalne przekazywane do endpointa
+    //@PathVariable - adnotacja określająca zmienną zawartą w ścieżce do endpointa, jest ona wymagana
+    //Metoda zwracająca liczbę pracowników potrzebnych na piętro, cały parking i koszt dziennego utrzymania parkingu
     @RequestMapping(method = RequestMethod.GET, value = "employees/spots/{spots}")
     public ResponseEntity<?> getNumberOfEmployeesForFloorsAndTheirDailySalary(
             @PathVariable("spots") Integer spotsToService,
